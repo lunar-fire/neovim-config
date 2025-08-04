c4337af95f78b232e1e9293ed728ccd759e13c21
lunar-fire <kennyabitbol@gmail.com>
2025-08-03 23:45:50 -0500

    updated keymaps

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,9 +1,9 @@
 return {
-	  'shaunsingh/nord.nvim',
-	  lazy = false,
-	  priority = 1000,
-	  config = function()
-		-- Example config in lua
+  'shaunsingh/nord.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Example config in lua
     vim.g.nord_contrast = true
     vim.g.nord_borders = false
     vim.g.nord_disable_background = false
@@ -13,5 +13,5 @@ return {
 
     -- Load the colorscheme
     require('nord').set()
-          end
-  }
+  end,
+}
